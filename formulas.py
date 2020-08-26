f425141b0d786dddd1aa26e54ecf67ea1915dadd
bsamaha <59106056+bsamaha@users.noreply.github.com>
2020-08-26 10:19:35 -0500

    added test_stationarity to formulas

--- formulas.py
+++ formulas.py
@@ -3,7 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from statsmodels.tsa.seasonal import STL
+from statsmodels.tsa.stattools import adfuller
 
+
+
+def test_stationarity(timeseries, window = 12, cutoff = 0.01):
+
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(window).mean()
+    rolstd = timeseries.rolling(window).std()
+
+    #Plot rolling statistics:
+    fig = plt.figure(figsize=(12, 8))
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
+    
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC', maxlag = 20 )
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    pvalue = dftest[1]
+    if pvalue < cutoff:
+        print('p-value = %.4f. The series is likely stationary.' % pvalue)
+    else:
+        print('p-value = %.4f. The series is likely non-stationary.' % pvalue)
+    
+    print(dfoutput)
 
 def plot_series(time, series, format="-", start=0, end=None, label=None):
     """[Plot the series data over a time range]
@@ -31,6 +62,8 @@ def moving_average_forecast(series, window_size):
   mov = np.cumsum(series)
   mov[window_size:] = mov[window_size:] - mov[:-window_size]
   return mov[window_size - 1:-1] / window_size
+
+
     
 
 
