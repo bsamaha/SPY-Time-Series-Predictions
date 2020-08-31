65f0cc577507fa19bfdee8cfec99c1d81d84637e
bsamaha <59106056+bsamaha@users.noreply.github.com>
2020-08-31 15:36:41 -0500

    finished rnn

--- formulas.py
+++ formulas.py
@@ -6,6 +6,28 @@ from statsmodels.tsa.seasonal import STL
 from statsmodels.tsa.stattools import adfuller
 import tensorflow as tf
 
+
+def seq2seq_window_dataset(series, window_size, batch_size=32,
+                           shuffle_buffer=1000):
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[1:]))
+    return ds.batch(batch_size).prefetch(1)
+
+def window_dataset(series, window_size, batch_size=32,
+                   shuffle_buffer=1000):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer)
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+
 def model_forecast(model, series, window_size):
     ds = tf.data.Dataset.from_tensor_slices(series)
     ds = ds.window(window_size, shift=1, drop_remainder=True)
@@ -72,15 +94,7 @@ def moving_average_forecast(series, window_size):
 
 
     
-def window_dataset(series, window_size, batch_size=32,
-                   shuffle_buffer=1000):
-    dataset = tf.data.Dataset.from_tensor_slices(series)
-    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
-    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
-    dataset = dataset.shuffle(shuffle_buffer)
-    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
-    dataset = dataset.batch(batch_size).prefetch(1)
-    return dataset
+
 
 
 def calculate_returns(close):
